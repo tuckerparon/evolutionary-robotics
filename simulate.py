aebc232e9b5684237c1164c4a4f5e11d253dabfb
Tucker Paron <91924738+tuckerparon@users.noreply.github.com>
2023-01-21 22:19:27 -0500

    Included time steps and sleep function.

--- simulate.py
+++ simulate.py
@@ -6,7 +6,21 @@ CS206: Evolutionary Robotics
 @author: tuckerparon
 """
 
+# imports
 import pybullet as p
+import time
 
+
+# create physics client
 physicsClient = p.connect(p.GUI)
+
+# update simulation across 1000 time steps
+for i in range(1000):
+    
+    print(i)
+    time.sleep(1/60)
+    p.stepSimulation()
+    
+
+# disconnect
 p.disconnect()
