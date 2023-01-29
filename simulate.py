86b7b84e3c903c86295c54cb70a27f8c72fc000b
Tucker Paron <91924738+tuckerparon@users.noreply.github.com>
2023-01-29 18:32:04 -0500

    Adding sensors
    Adding a sensor to the front and back legs, recording the sensor values in an array, and saving the arrays in a .npy file.

--- simulate.py
+++ simulate.py
@@ -11,6 +11,9 @@ CS206: Evolutionary Robotics
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy
+import sys
 
 # create physics client
 physicsClient = p.connect(p.GUI)
@@ -28,13 +31,29 @@ robotId = p.loadURDF("body.urdf")
 # read in described world
 p.loadSDF("world.sdf")
 
+# prepare to simulate
+pyrosim.Prepare_To_Simulate(robotId)
+
+# create zero vectors to populate with sensor information
+backLegSensorValues = numpy.zeros(1000)
+frontLegSensorValues = numpy.zeros(1000)
+
 # update simulation across 1000 time steps
 for i in range(1000):
     
     print(i)
     time.sleep(1/60)
     p.stepSimulation()
-    
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg") # back leg sensor
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg") # back leg sensor
 
 # disconnect
 p.disconnect()
+
+# print 
+print(backLegSensorValues)
+print(frontLegSensorValues)
+
+# save sensor arrays
+numpy.save("data/backLegSensorValues.npy", backLegSensorValues)
+numpy.save("data/frontLegSensorValues.npy", frontLegSensorValues)
