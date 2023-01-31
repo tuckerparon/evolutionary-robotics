ba0841723f6d1fbb8595637c40e1ea007c4776e9
Tucker Paron <91924738+tuckerparon@users.noreply.github.com>
2023-01-30 21:56:39 -0500

    Add angle and control vectors.

--- analyze.py
+++ analyze.py
@@ -15,12 +15,25 @@ import matplotlib.pyplot as plt
 backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
 frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
 
+# load in angle/control vectors
+targetAngles =  numpy.load('data/targetAngles.npy')
+backLegMotorValues =  numpy.load('data/backLegMotorValues.npy')
+frontLegMotorValues =  numpy.load('data/frontLegMotorValues.npy')
+
 # print sensor values
-print(backLegSensorValues)
-print(frontLegSensorValues)
+#print(backLegSensorValues)
+#print(frontLegSensorValues)
+
+# print angle/control values
+#print(targetAngles)
+#print(backLegMotorValues)
+#print(frontLegMotorValues)
 
 # plot sensor values
-plt.plot(backLegSensorValues, label='Back Leg', linewidth=1)
-plt.plot(frontLegSensorValues, label='Front Leg', linewidth=1)
+#plt.plot(backLegSensorValues, label='Back Leg', linewidth=1)
+#plt.plot(frontLegSensorValues, label='Front Leg', linewidth=1)
+#plt.plot(targetAngles, label='Angles', linewidth=1)
+plt.plot(backLegMotorValues, label='Back Leg', linewidth=1)
+plt.plot(frontLegMotorValues, label='Front Leg', linewidth=1)
 plt.legend()
 plt.show()
