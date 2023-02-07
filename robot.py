ac142bdfb5aa965e8065889e28c79e28efff75f7
Tucker Paron <91924738+tuckerparon@users.noreply.github.com>
2023-02-07 18:35:31 -0500

    encode jointName in ASCII
    This put the string in the following format: b'Torso_BackLeg' which allows the joints to be accessed

--- robot.py
+++ robot.py
@@ -55,7 +55,7 @@ class ROBOT:
 
         for neuronName in self.nn.Get_Neuron_Names():
             if self.nn.Is_Motor_Neuron(neuronName):
-                jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
+                jointName = self.nn.Get_Motor_Neurons_Joint(neuronName).encode('ASCII')
                 desiredAngle = self.nn.Get_Value_Of(neuronName)
                 self.motors[jointName].Set_Value(self.robot, desiredAngle)
 
