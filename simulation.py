ba5f64136c627c8ea01f5c5fe93e25a73ed0007a
Tucker Paron <91924738+tuckerparon@users.noreply.github.com>
2023-02-07 18:34:12 -0500

    remove t from Act()

--- simulation.py
+++ simulation.py
@@ -40,7 +40,7 @@ class SIMULATION:
             p.stepSimulation()
             self.robot.Sense(t)
             self.robot.Think()
-            self.robot.Act(t)
+            self.robot.Act()
 
 
     def __del__(self):
