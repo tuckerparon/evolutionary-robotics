319ac4c7ad00a1179c3c25dff2c87767e92c11ca
Tucker Paron <91924738+tuckerparon@users.noreply.github.com>
2023-02-07 18:03:49 -0500

    switch t for desired angle.
    1. switch t for desired angle.
    2. Delete prepare_to_act function

--- motor.py
+++ motor.py
@@ -19,34 +19,8 @@ class MOTOR:
 
         # set joint name and get ready to act
         self.jointName = jointName
-        self.Prepare_To_Act()
 
-    def Prepare_To_Act(self):
-
-        # initialize motor value array
-        self.motorValues = numpy.zeros(c.time_steps)
-
-        # retrieve motor constants for calculation
-        self.amplitude = c.amplitude
-        self.frequency = c.frequency
-        self.offset = c.phaseOffset
-        self.targetAngles = c.targetAngles
-
-        # line 105 in refactoring instructions - make one motor twice as fast
-        if self.jointName == b'Torso_BackLeg':
-            self.frequency *= 2
-
-        # calculate motor values
-        for i in range(c.time_steps):
-            self.motorValues[i] = self.amplitude * numpy.sin(self.frequency * self.targetAngles[i] + self.offset)
-
-
-    def Set_Value(self, robot, t):
+    def Set_Value(self, robot, desiredAngle):
 
         # set motor values for appropriate joint
-        pyrosim.Set_Motor_For_Joint(bodyIndex = robot, jointName = self.jointName, controlMode = p.POSITION_CONTROL, targetPosition = self.motorValues[t], maxForce = 50)
-
-    def Save_Values(self):
-
-        # save values
-        numpy.save('data//motorValues.npy', self.motorValues)
+        pyrosim.Set_Motor_For_Joint(bodyIndex = robot, jointName = self.jointName, controlMode = p.POSITION_CONTROL, targetPosition = desiredAngle, maxForce = 50)
