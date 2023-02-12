9d9a7c421b724f90668be92774ea5a627dccb420
Tucker Paron <91924738+tuckerparon@users.noreply.github.com>
2023-02-12 14:39:56 -0500

    Add files via upload

---
+++ search.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Search
+CS206: Evolutionary Robotics
+
+@author: tuckerparon
+"""
+
+import os
+
+for i in range(5):
+    os.system("python3 generate.py")
+    os.system("python3 simulate.py")
